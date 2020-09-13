77891c71d8813b390556d6dfc49a8fa22b8e1774
Arnold Perelló <65364339+arnoldfini@users.noreply.github.com>
2020-09-13 15:55:08 +0200

    Fix bugs + draw plots

---
+++ form-data/avg-data/avg_3dgraph.py
@@ -0,0 +1,61 @@
+import matplotlib.pyplot as plt
+from numpy.random import random
+from avgdata import df_avg, getTitles
+import math
+from mpl_toolkits.mplot3d import Axes3D
+
+# Dataframe, and songs' titles
+df = df_avg()
+titles = getTitles()
+
+# Array where all the coordinates and its respective labels will be stored
+x_values = []
+y_values = []
+z_values = []
+actual_titles = []
+
+# Declare "graph"
+fig = plt.figure()
+ax = plt.subplot(111, projection='3d')
+
+# Axis names
+ax.set_xlabel('Positivitat [cos(x)]', fontsize=10)
+ax.set_ylabel('Intensitat [sin(x)]', fontsize=10)
+ax.set_zlabel('Trascendent', fontsize=10)
+
+# Random values declared in avg_graph.py, so they have the same plots.
+random_values = [12, 34, 30, 14, 24, 26, 19, 26, 25, 32, 34, 1, 15, 35, 18, 32, 12, 27, 24, 13]
+
+# Loop iterating through de dataframe to store each value to its respective coordinate (x,y or z)
+for i in random_values:
+    x_values.append(df['Feelings'][i][0])
+    y_values.append(df['Feelings'][i][1])
+    z_values.append(df['Transcendent'][i])
+    actual_titles.append(titles[i])
+
+# Plot the values, using the three arrays
+ax.scatter(x_values, y_values, z_values)
+
+# Name each point
+for x, y, z, label in zip(x_values, y_values, z_values, actual_titles):
+    ax.text(x, y, z, label)
+
+# Move left y-axis and bottom x-axis to center of the figure
+ax.plot((0, 0), (-1, 1), 'k--')
+ax.plot((-1, 1), (0, 0), 'k--')
+
+# UNIT CIRCLE PLOT
+sin = []
+cos = []
+
+# Append all values so they form an unit circle
+for i in range(0, 360):
+    cos.append(math.cos(i))
+    sin.append(math.sin(i))
+
+# Plot unit circle
+ax.plot(cos, sin, 'k--', linewidth=0.1)
+
+# Title and show
+plt.title('Emocions entre cançons')
+plt.show()
--- form-data/avg-data/avg_graph.py
+++ form-data/avg-data/avg_graph.py
@@ -2,18 +2,17 @@ import matplotlib.pyplot as plt
 from avgdata import df_avg, getTitles
 from emotions import emodict, emodictrans
 import numpy as np
-from mpl_toolkits.mplot3d import Axes3D
 
+# Declare Dataframe, titles, and emotions dictionaries
 df = df_avg()
 titles = getTitles()
 emotions = emodict()
 emotrans = emodictrans()
 
+# Array where all the coordinates and its respective labels will be stored
 x_values = []
 y_values = []
 colour_sequence = []
-
-
 
 # Axis
 fig1 = plt.figure()
@@ -21,12 +20,11 @@ ax1 = fig1.add_subplot(1,1,1)
 ax1.set_xbound(-1,1)
 ax1.set_ybound(-1,1)
 
-
 # Move left y-axis and bottom x-axis to center of the figure
 plt.plot((0, 0), (-1, 1), 'k--')
 plt.plot((-1, 1), (0, 0), 'k--')
 
-#   Unit circle
+#  Unit circle
 ax = fig1.add_subplot(1, 1, 1)
 circ = plt.Circle((0, 0), radius=1, edgecolor='k', facecolor='None')
 ax.add_patch(circ)
@@ -35,13 +33,21 @@ ax.add_patch(circ)
 trans_songs = [[], []]
 no_trans_songs = [[], []]
 
-for i in range(20):
+# Array to store the random values if useful later
+random_values = [12, 34, 30, 14, 24, 26, 19, 26, 25, 32, 34, 1, 15, 35, 18, 32, 12, 27, 24, 13]
+i=0
+for j in random_values:
+#for i in range(len(df)):
 
-    j = int(len(df) * np.random.random_sample())
+    # Create random number to iterate random rows in the dataframe, and store it in random_values[]
+    #j = int(len(df) * np.random.random_sample())
+    #random_values.append(j)
 
+    # Append both x and y coordinates
     x_values.append(df['Feelings'][j][0])
     y_values.append(df['Feelings'][j][1])
 
+    # However, if column df['Transcendent'][j] is 1, the colour must be read, so it's distinguishable
     if df['Transcendent'][j] == 1:
         colour_sequence.append('red')
         plt.annotate(titles[j], (x_values[i], y_values[i]))
@@ -53,24 +59,27 @@ for i in range(20):
         plt.annotate(titles[j], (x_values[i], y_values[i]))
         no_trans_songs[0].append(x_values[i])
         no_trans_songs[1].append(y_values[i])
+    i+=1
 
-
-
+# Draw points
 plt.scatter(trans_songs[0], trans_songs[1], c='r', label='Música trascendent')
-plt.scatter(no_trans_songs[0], no_trans_songs[1], c='b')
-#plt.legend(legend_val, 'Trascendent', loc='lower left', ncol=3, fontsize=8, scatterpoints=1)
-#plt.legend(handles=scatter.legend_elements()[0], labels='Trascendent')
-plt.legend(loc='upper left',
-          fancybox=True, shadow=True, ncol=4)
+plt.scatter(no_trans_songs[0], no_trans_songs[1], label='Música no trascendent')
 
+# Legend the labels above
+plt.legend(loc='upper left',
+          fancybox=True, shadow=True, ncol=1)
+
+# Label the emotions in a 2D plane, which they lie in the unit circle
 for key, value in emotions.items():
     plt.annotate(emotrans[key], value)
 
+# Draw emotion points
+plt.scatter([v[0] for k, v in emotions.items()], [v[1] for k,v in emotions.items()], c='k')
+plt.scatter(x_values, y_values, c=colour_sequence)
+
+# Name axis
 plt.xlabel('Positivitat [cos(x)]')
 plt.ylabel('Intensitat [sin(x)]')
 
-
-
 plt.title('Emocions entre cançons ')
-plt.scatter(x_values, y_values, c=colour_sequence)
 plt.show()
