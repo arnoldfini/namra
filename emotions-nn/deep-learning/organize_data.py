fffdbdcde6d3258bbdddbe525eabde0a02be2a17
Arnold Perelló <65364339+arnoldfini@users.noreply.github.com>
2020-09-23 07:18:13 +0200

    Added labels

--- emotions-nn/deep-learning/organize_data.py
+++ emotions-nn/deep-learning/organize_data.py
@@ -1,6 +1,8 @@
 import numpy as np
-from ground_truth import get_ground_truth
+import pandas as pd
+from ground_truth import compute_ground_truth
 from test_data import test_dataset
+from avgdata import df_avg
 from input import extract_features, get_id
 
 
@@ -19,7 +21,19 @@ def data():
     # Test data
     test_data = test_dataset()
 
-    # Ground truth
-    output_data = get_ground_truth()
+    return train_data, val_data, test_data
 
-    return train_data, val_data, test_data, output_data
+
+def labels():
+
+    df = df_avg()
+
+    # Compute one hot encoding of the average stored in df_avg
+    df_train = df.drop(df.index[[range(7)]])
+    df_val = df.drop(df.index[[range(7, len(df))]])
+
+    # This will be the labels of the NN
+    train_labels = compute_ground_truth(df_train)
+    val_labels = compute_ground_truth(df_val)
+
+    return train_labels, val_labels
