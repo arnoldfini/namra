44727442aee8ed8546d5900f5a23710d4fcc24cc
Arnold Perelló <65364339+arnoldfini@users.noreply.github.com>
2020-09-23 07:17:44 +0200

    Model now working

--- emotions-nn/deep-learning/model.py
+++ emotions-nn/deep-learning/model.py
@@ -2,15 +2,20 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.layers import InputLayer
 import numpy as np
-from organize_data import data
+from organize_data import data, labels
+from emotions import emodict
 import matplotlib.pyplot as plt
 
 '''
 File of the actual neural network model
 '''
 
+# Emotions
+emotions = emodict()
+
 # Datasets
-train_data, val_data, test_data, output_data = data()
+train_data, val_data, test_data = data()
+train_labels, val_labels = labels()
 
 seed = 7
 np.random.seed(seed)
@@ -20,17 +25,17 @@ model = Sequential()
 model.add(InputLayer(input_shape=(518, )))
 model.add(Dense(512, activation='relu'))
 model.add(Dense(512, activation='relu'))
-model.add(Dense(len(output_data), activation='sigmoid'))
+model.add(Dense(len(emotions), activation='sigmoid'))
 
 print(model.summary)
 
-model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 # Fit the model
-history = model.fit(input_data, output_data, epochs=10, batch_size=64, verbose=0)
+history = model.fit(train_data, train_labels, epochs=1000, batch_size=64, verbose=0)
 
 # evaluate the model
-loss, accuracy = model.evaluate(input_data, output_data)
+loss, accuracy = model.evaluate(val_data, val_labels)
 
 print(f'Loss of the neural network: {loss} \nAccuracy of the neural network: {accuracy*100}%')
 
